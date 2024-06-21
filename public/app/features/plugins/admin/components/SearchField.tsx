f99ec373697717c18a9c59f00b722a02a2bb1193
google-boy <otienoh13@gmail.com>
2024-06-21 08:58:15 +0300

    Update plugins search placeholder text

--- public/app/features/plugins/admin/components/SearchField.tsx
+++ public/app/features/plugins/admin/components/SearchField.tsx
@@ -40,7 +40,7 @@ export const SearchField = ({ value, onSearch }: Props) => {
           onSearch(e.currentTarget.value);
         }
       }}
-      placeholder="Search Grafana plugins"
+      placeholder="Search eyeMonitor plugins or extensions"
       onChange={(value) => {
         setQuery(value);
       }}
